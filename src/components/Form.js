132a2c5246d5d844d1f1dbd8400bb9925061e558
Fatimah <fatimah@Fatimahs-MBP.broadband>
2019-10-21 14:45:04 +0100

    added css to form and layed out further grids within

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,10 +4,17 @@ const Form = props => {
   const { getWeather } = props;
   return (
     <div className="form">
-      <p>Find the temperature conditions in your area!</p>
+      <p>Find the weather conditions in your area!</p>
       <form onSubmit={getWeather}>
-        <input type="text" name="city" placeholder="City..." />
-        <input type="text" name="country" placeholder="Country..." />
+        <input type="text" name="city" placeholder="City..." className="city" />
+
+        <input
+          type="text"
+          name="country"
+          placeholder="Country..."
+          className="country"
+        />
+
         <button>Get Weather</button>
       </form>
     </div>
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,15 +4,18 @@ const Weather = props => {
   const { temperature, city, country, humidity, description, error } = props;
   return (
     <div className="results">
-      {city && country ? (
-        <p>
-          Location: {city}, {country}
-        </p>
-      ) : null}
-      {description ? <p>Conditions: {description}</p> : null}
-      {temperature ? <p>Temperature: {temperature}</p> : null}
-      {humidity ? <p>Humidity: {humidity}</p> : null}
-      {error ? <p>{error}</p> : null}
+      <div className="info">
+        {city && country ? (
+          <p>
+            Location: {city}, {country}
+          </p>
+        ) : null}
+        {description ? <p>Conditions: {description}</p> : null}
+        {temperature ? <p>Temperature: {temperature}</p> : null}
+        {humidity ? <p>Humidity: {humidity}</p> : null}
+        {error ? <p>{error}</p> : null}
+      </div>
+      <p className="pic">**place pic here**</p>
     </div>
   );
 };
